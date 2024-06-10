3ac9b0c9c14078e6d70feab784f4ef55e48ae0ea
james <jamesjulian974138@gmail.com>
2024-06-10 10:57:58 -0400

    3 problems 6/9

---
+++ leetcode-practice/easy/207-contains-duplicate/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> duplicate = new HashSet<>();
+
+        for(int num : nums){
+            if(!duplicate.add(num)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ leetcode-practice/medium/128-longest-consecutive-sequence/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+
+        for (int i: nums){
+            hs.add(i);
+        }
+        int c =0;
+
+        for (int x:hs){
+            if(!hs.contains(x-1)){
+                int y = x+1;
+                while(hs.contains(y)){
+                    y++;
+                }
+                c= Math.max(c,y-x);
+            }
+        }
+        return c;
+    }
+}
